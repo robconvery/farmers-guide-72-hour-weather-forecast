1bc16386d01b1de7a029659ad15e4549a5fa763e
Robert Convery <robconvery@me.com>
2018-12-17 22:25:23 +0000

    Created `fake weather gateway`

---
+++ src/FakeWeatherGateway.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Class FakeWeatherGateway
+ *
+ * @package Robconvery\FarmersGuideForecast\Interfaces
+ * @author robconvery <robconvery@me.com>
+ */
+
+namespace Robconvery\FarmersGuideForecast;
+
+use Illuminate\Support\Collection;
+use Robconvery\FarmersGuideForecast\Interfaces\GatewayInterface;
+
+class FakeWeatherGateway implements GatewayInterface
+{
+    /**
+     * @param string $postcode
+     * @return GatewayInterface
+     */
+    public function getForecast(string $postcode): GatewayInterface
+    {
+        return $this;
+    }
+
+    /**
+     * @return Collection
+     */
+    public function extract(): Collection
+    {
+        return collect(static::testData())->map(function ($row) {
+            return collect($row);
+        });
+    }
+
+    /**
+     * @return array
+     */
+    public static function testData(): array
+    {
+        return include dirname(__DIR__) . '/config/test_data.php';
+    }
+}
